f21ad555a57b113578ca562aabe1b80034659db9
DESKTOP-057S5TM\2mail <chiranga.yasanthi92@gmail.com>
2020-08-12 23:54:22 +1000

    test-basic changes have been made. need to check again.

--- src/library/entities/Calendar.java
+++ src/library/entities/Calendar.java
@@ -4,63 +4,63 @@ import java.util.concurrent.TimeUnit;
 
 public class Calendar {
 	
-	private static Calendar sElF;
-	private static java.util.Calendar cAlEnDaR;
+	private static Calendar self; //changed variable "sElF" to "self"
+	private static java.util.Calendar calendar;//changed variable "cAlEnDaR" to "calendar"
 	
 	
 	private Calendar() {
-		cAlEnDaR = java.util.Calendar.getInstance();
+		calendar = java.util.Calendar.getInstance();//changed "cAlEnDaR" to "calendar"
 	}
 	
-	public static Calendar gEtInStAnCe() {
-		if (sElF == null) {
-			sElF = new Calendar();
+	public static Calendar getInstance() { //changed "gEtInStAnCe" to "getInstance"
+		if (self == null) {
+			self = new Calendar();
 		}
-		return sElF;
+		return self;
 	}
 	
 	public void incrementDate(int days) {
-		cAlEnDaR.add(java.util.Calendar.DATE, days);		
+		calendar.add(java.util.Calendar.DATE, days);//changed "cAlEnDaR" to "calendar"		
 	}
 	
-	public synchronized void SeT_DaTe(Date DaTe) {
+	public synchronized void setDate(Date Date) {//changed  "SeT_DaTe" to "setDate" & //changed  "Date DaTe" to "Date Date"
 		try {
-			cAlEnDaR.setTime(DaTe);
-	        cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-	        cAlEnDaR.set(java.util.Calendar.MINUTE, 0);  
-	        cAlEnDaR.set(java.util.Calendar.SECOND, 0);  
-	        cAlEnDaR.set(java.util.Calendar.MILLISECOND, 0);
+			calendar.setTime(Date);//changed  "cAlEnDaR" to "calendar" & //changed  "DaTe" to "Date"
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  //changed  "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MINUTE, 0); //changed  "cAlEnDaR" to "calendar" 
+	        calendar.set(java.util.Calendar.SECOND, 0);  //changed  "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MILLISECOND, 0);//changed  "cAlEnDaR" to "calendar"
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
 		}	
 	}
-	public synchronized Date gEt_DaTe() {
+	public synchronized Date getDate() {//changed  "gEt_DaTe" to "getDate"
 		try {
-	        cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-	        cAlEnDaR.set(java.util.Calendar.MINUTE, 0);  
-	        cAlEnDaR.set(java.util.Calendar.SECOND, 0);  
-	        cAlEnDaR.set(java.util.Calendar.MILLISECOND, 0);
-			return cAlEnDaR.getTime();
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);//changed "cAlEnDaR" to "calendar"  
+	        calendar.set(java.util.Calendar.MINUTE, 0);  //changed "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.SECOND, 0);  //changed "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MILLISECOND, 0);//changed "cAlEnDaR" to "calendar"
+			return calendar.getTime();
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
 		}	
 	}
 
-	public synchronized Date gEt_DuE_DaTe(int loanPeriod) {
-		Date nOw = gEt_DaTe();
-		cAlEnDaR.add(java.util.Calendar.DATE, loanPeriod);
-		Date dUeDaTe = cAlEnDaR.getTime();
-		cAlEnDaR.setTime(nOw);
-		return dUeDaTe;
+	public synchronized Date getDueDate(int loanPeriod) {//changed "gEt_DuE_DaTe" to "getDueDate"
+		Date now = getDate();//changed  "gEt_DaTe" to "getDate" & changed "nOw" to "now"
+		calendar.add(java.util.Calendar.DATE, loanPeriod);
+		Date duedate = calendar.getTime();//changed "dUeDaTe" to "duedate"
+		calendar.setTime(now);//changed "nOw" to "now"
+		return duedate;//changed "dUeDaTe" to "duedate"
 	}
 	
-	public synchronized long GeT_DaYs_DiFfErEnCe(Date targetDate) {
+	public synchronized long getDaysDifferrence(Date targetDate) {//changed "GeT_DaYs_DiFfErEnCe" to "getDaysDifferrence"
 		
-		long Diff_Millis = gEt_DaTe().getTime() - targetDate.getTime();
-	    long Diff_Days = TimeUnit.DAYS.convert(Diff_Millis, TimeUnit.MILLISECONDS);
-	    return Diff_Days;
+		long diffMillis = getDate().getTime() - targetDate.getTime();//changed  "gEt_DaTe" to "getDate" & changed "Diff_Mills" to "diffMillis"
+	    long diffDay = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS);// changed "Diff_Day" to "diffDay"
+	    return diffDay;
 	}
 
 }
