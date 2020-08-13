79dba54ae3125c14ad8cdbb305353c035edc8bc5
DESKTOP-057S5TM\2mail <chiranga.yasanthi92@gmail.com>
2020-08-13 18:22:09 +1000

    Changed the code and added comments

--- src/library/entities/Calendar.java
+++ src/library/entities/Calendar.java
@@ -2,17 +2,22 @@ package library.entities;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+// Author   : Chiranga
+// Reviwer  : Subhashini
+// Mediator : Nipuna
+
+
 public class Calendar {
 	
-	private static Calendar self; //changed variable "sElF" to "self"
-	private static java.util.Calendar calendar;//changed variable "cAlEnDaR" to "calendar"
+	private static Calendar self; // changed variable "sElF" to "self"
+	private static java.util.Calendar calendar; // changed variable "cAlEnDaR" to "calendar"
 	
 	
 	private Calendar() {
-		calendar = java.util.Calendar.getInstance();//changed "cAlEnDaR" to "calendar"
+		calendar = java.util.Calendar.getInstance(); // changed method "cAlEnDaR" to "calendar"
 	}
 	
-	public static Calendar getInstance() { //changed "gEtInStAnCe" to "getInstance"
+	public static Calendar getInstance() { // changed method "gEtInStAnCe" to "getInstance"
 		if (self == null) {
 			self = new Calendar();
 		}
@@ -20,27 +25,27 @@ public class Calendar {
 	}
 	
 	public void incrementDate(int days) {
-		calendar.add(java.util.Calendar.DATE, days);//changed "cAlEnDaR" to "calendar"		
+		calendar.add(java.util.Calendar.DATE, days); // changed method "cAlEnDaR" to "calendar"		
 	}
 	
-	public synchronized void setDate(Date Date) {//changed  "SeT_DaTe" to "setDate" & //changed  "Date DaTe" to "Date Date"
+	public synchronized void setDate(Date date) { // changed method "SeT_DaTe" to "setDate" & // changed  "Date DaTe" to "Date Date"
 		try {
-			calendar.setTime(Date);//changed  "cAlEnDaR" to "calendar" & //changed  "DaTe" to "Date"
-	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  //changed  "cAlEnDaR" to "calendar"
-	        calendar.set(java.util.Calendar.MINUTE, 0); //changed  "cAlEnDaR" to "calendar" 
-	        calendar.set(java.util.Calendar.SECOND, 0);  //changed  "cAlEnDaR" to "calendar"
-	        calendar.set(java.util.Calendar.MILLISECOND, 0);//changed  "cAlEnDaR" to "calendar"
+			calendar.setTime(Date);// changed method "cAlEnDaR" to "calendar" & // changed  "DaTe" to "Date"
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  // changed method "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MINUTE, 0); // changed method "cAlEnDaR" to "calendar" 
+	        calendar.set(java.util.Calendar.SECOND, 0);  // changed method "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MILLISECOND, 0); // changed method "cAlEnDaR" to "calendar"
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
 		}	
 	}
-	public synchronized Date getDate() {//changed  "gEt_DaTe" to "getDate"
+	public synchronized Date getDate() { // changed method "gEt_DaTe" to "getDate"
 		try {
-	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);//changed "cAlEnDaR" to "calendar"  
-	        calendar.set(java.util.Calendar.MINUTE, 0);  //changed "cAlEnDaR" to "calendar"
-	        calendar.set(java.util.Calendar.SECOND, 0);  //changed "cAlEnDaR" to "calendar"
-	        calendar.set(java.util.Calendar.MILLISECOND, 0);//changed "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0); // changed method "cAlEnDaR" to "calendar"  
+	        calendar.set(java.util.Calendar.MINUTE, 0);  // changed method "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.SECOND, 0);  // changed method "cAlEnDaR" to "calendar"
+	        calendar.set(java.util.Calendar.MILLISECOND, 0);// changed method "cAlEnDaR" to "calendar"
 			return calendar.getTime();
 		}
 		catch (Exception e) {
@@ -48,18 +53,18 @@ public class Calendar {
 		}	
 	}
 
-	public synchronized Date getDueDate(int loanPeriod) {//changed "gEt_DuE_DaTe" to "getDueDate"
-		Date now = getDate();//changed  "gEt_DaTe" to "getDate" & changed "nOw" to "now"
+	public synchronized Date getDuedate(int loanPeriod) { // changed method "gEt_DuE_DaTe" to "getDuedate"
+		Date now = getDate(); // changed method "gEt_DaTe" to "getDate" & changed "nOw" to "now"
 		calendar.add(java.util.Calendar.DATE, loanPeriod);
-		Date duedate = calendar.getTime();//changed "dUeDaTe" to "duedate"
-		calendar.setTime(now);//changed "nOw" to "now"
-		return duedate;//changed "dUeDaTe" to "duedate"
+		Date duedate = calendar.getTime(); //changed method "dUeDaTe" to "duedate"
+		calendar.setTime(now); // changed method "nOw" to "now"
+		return duedate; // changed method "dUeDaTe" to "duedate"
 	}
 	
-	public synchronized long getDaysDifferrence(Date targetDate) {//changed "GeT_DaYs_DiFfErEnCe" to "getDaysDifferrence"
+	public synchronized long getDaysDifferrence(Date targetDate) { // changed method "GeT_DaYs_DiFfErEnCe" to "getDaysDifferrence"
 		
-		long diffMillis = getDate().getTime() - targetDate.getTime();//changed  "gEt_DaTe" to "getDate" & changed "Diff_Mills" to "diffMillis"
-	    long diffDay = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS);// changed "Diff_Day" to "diffDay"
+		long diffMillis = getDate().getTime() - targetDate.getTime(); // changed method "gEt_DaTe" to "getDate" & changed "Diff_Mills" to "diffMillis"
+	    long diffDay = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS); // changed method "Diff_Day" to "diffDay"
 	    return diffDay;
 	}
 
