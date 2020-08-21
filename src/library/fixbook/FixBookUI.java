c11b122378c3282be3aede30c50fcb45901cc6fd
Subhashini90 <68838368+Subhashini90@users.noreply.github.com>
2020-08-21 23:00:43 +1000

    Update FixBookUI.java
    Updated missing curly brackets and variable name of "UiState"

--- src/library/fixbook/FixBookUI.java
+++ src/library/fixbook/FixBookUI.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class FixBookUI {  
 
-<<<<<<< HEAD
+
 	public static enum UiState { INITIALISED, READY, FIXING, COMPLETED }; // Changed the enum name "uI_sTaTe" to "UiState"
 
 	private FixBookcontrol control;  // Changed the variable name "CoNtRoL"  to "control"  // changed the variable type to "Fix_Book_control"
@@ -20,7 +20,7 @@ public class FixBookUI {
 		this.control = control;
 		input = new Scanner(System.in);  // changed the variable name "InPuT" to "input"
 		state = UiState.INITIALISED;  // Changed the enum name "uI_sTaTe" to "UiState"  // Changed the variable name "StAtE" to "state"
-=======
+
 	public static enum UiState { INITIALISED, READY, FIXING, COMPLETED }; // Changed the enum name "uI_sTaTe" to "Ui_State"
 
 	private FixBookControl control;  // Changed the variable name "CoNtRoL"  to "control"  // changed the variable type to "Fix_Book_control"
@@ -32,7 +32,6 @@ public class FixBookUI {
 		this.control = control;
 		input = new Scanner(System.in);  // changed the variable name "InPuT" to "input"
 		state = UiState.INITIALISED;  // Changed the enum name "uI_sTaTe" to "Ui_State"  // Changed the variable name "StAtE" to "state"
->>>>>>> bb26a4ad932815ed17a2deda6d6edc42756f3b3e
 		control.setUi(this);  // Changed the variable name "CoNtRoL"  to "control"  // Changed the method name "SeT_Ui"  to "setUi"
 	}
 
@@ -50,27 +49,27 @@ public class FixBookUI {
 			switch (state) {  // Changed the variable name "StAtE" to "state"
 			
 			case READY:
-<<<<<<< HEAD
+
 				String bookEntryString = input("Scan Book (<enter> completes): ");  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"  // Changed the method type "iNpUt" to "input"
-				if (bookEntryString.length() == 0)  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"
-				{
+				if (bookEntryString.length() == 0){  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"
+				
 					control.scanningComplete();  // Changed the variable name "CoNtRoL"  to "control"  // Changed the method name "SCannING_COMplete" to "scanningComplete"
-=======
+
 				String bookEntryString = input("Scan Book (<enter> completes): ");  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"
 				if (bookEntryString.length() == 0){  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"
 				
 					control.scanningComplete();  // Changed the variable name "CoNtRoL"  to "control"  // Changed the method name "SCannING_COMplete" to "Scanning_Complete"
->>>>>>> bb26a4ad932815ed17a2deda6d6edc42756f3b3e
+
 				}
 				
 				else {
 					try {
 						int bookId = Integer.valueOf(bookEntryString).intValue();  // Changed the variable name  "BoOk_EnTrY_StRiNg"  to "bookEntryString"  // changed the variable name "BoOk_Id" to "bookId"
-<<<<<<< HEAD
+
 						control.bookScanned(bookId);  // Changed the variable name "CoNtRoL"  to "control"  // changed the variable name "BoOk_Id" to "bookId" // Changed the method name "BoOk_ScAnNeD" to "bookScanned"
-=======
+
 						control.bookScanned(bookId);  // Changed the variable name "CoNtRoL"  to "control"  // changed the variable name "BoOk_Id" to "bookId"
->>>>>>> bb26a4ad932815ed17a2deda6d6edc42756f3b3e
+
 					}
 					catch (NumberFormatException e) {
 						output("Invalid bookId");  // Changed the method name "OuTpUt" to "output"
@@ -81,12 +80,12 @@ public class FixBookUI {
 			case FIXING:
 				String ans = input("Fix Book? (Y/N) : ");  // Changed the variable name "AnS" to "ans" // changed the method name "iNpUt" to "input"
 				boolean fix = false;  // Changed the variable name "FiX" to "fix"
-				if (ans.toUpperCase().equals("Y"))  // Changed the variable name "AnS" to "ans"
+				if (ans.toUpperCase().equals("Y")){ // Changed the variable name "AnS" to "ans"
 					fix = true; // Changed the variable name "FiX" to "fix"
 				
 				control.fixBook(fix);  // Changed the variable name "CoNtRoL"  to "control"  // Changed the method name "FiX_BoOk" to fixBook  // Changed the variable name "FiX" to "fix"
 				break;
-								
+				}				
 			case COMPLETED:
 				output("Fixing process complete"); // Changed the method name "OuTpUt" to "output"
 				return;
